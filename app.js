fc745b889d4b53d312d6a6d32ab2eb31c65032cc
Prabhat Mani Srivastava <79709170+DarkM4tt@users.noreply.github.com>
2023-01-26 01:37:51 +0530

    Updated CORS

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(expressValidator())
 app.use(cors())
+app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      next();
+    });
 
 app.use('/api', authRoutes)
 app.use('/api', userRoutes)
